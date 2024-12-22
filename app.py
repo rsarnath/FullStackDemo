5b94c17e86c97058e3d7933d2551dcdbbb0a1d2d
rsarnath <136544306+rsarnath@users.noreply.github.com>
2024-12-22 23:37:21 +0000

    535PM22Dec

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
-arr = [];
+arr = ["book1"];
 
 # Route to render the index.html page
 @app.route('/')
@@ -16,8 +16,11 @@ def get_all_books_v2():
 # API to add a book to the database
 @app.route('/api/add_book', methods=['POST'])
 def add_book():
-     
+    print("adding book")
+    data = request.get_json()
+    title = data.get('title')
     arr.append(title);
+    return jsonify({'message': 'Book added successfully'})
  
 
 if __name__ == '__main__':
